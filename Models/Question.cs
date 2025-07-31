2b3c5f2a9d92a83e698fb98a0b5be96c82358593
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-07-31 10:33:39 +0800

    models question

---
+++ Models/Question.cs
@@ -0,0 +1,12 @@
+
+
+public class Question
+{
+    public string? QuestionText { get; set; }
+    public string[]? Choices { get; set; }
+
+    public string? Tag { get; set; }
+
+    public string? CorrectAnswer { get; set; }
+
+}
